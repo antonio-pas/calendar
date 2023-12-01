16fd4f5cd524b48664e5e2ce688b62872a6648b3
antonio-pas <antonio.b.passos18@gmail.com>
2023-11-30 22:56:28 -0600

    Initial commit

---
+++ components/Calendar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Today is {{ date }}</h1>
+    <p>There are {{ daysInMonth }}</p>
+    <p>There are {{ preDays }} from Sunday until the start of the month</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+const date = useState('date', () => new Date(Date.now()))
+const daysInMonth = new Date(date.value.getFullYear(), date.value.getMonth(), 0).getDate();
+const preDays = date.value.getDay();
+</script>
